02a186ca45d9e3b8bd852684a6a498b713e6bc57
saeid1994 <saeedsnaid@gmail.com>
2023-03-03 23:15:45 +0330

    adding font and color

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c9913d654e9279f9"; }
+/******/ 		__webpack_require__.h = function() { return "290f45986a3d01a2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,17 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        Cyan: "hsl(180, 66%, 49%)",
+        Dark_Violet: "hsl(257, 27%, 26%)",
+        Red: "hsl(0, 87%, 67%)",
+        Gray: "hsl(0, 0%, 75%)",
+        Grayish_Violet: "hsl(257, 7%, 63%)",
+        Very_Dark_Blue: "hsl(255, 11%, 22%)",
+        Very_Dark_Violet: "hsl(260, 8%, 14%)",
+      },
+    },
   },
   plugins: [],
 };
